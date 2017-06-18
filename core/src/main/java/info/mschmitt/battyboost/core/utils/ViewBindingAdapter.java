dbffe36ce588ca8475449a65ca628b8caf821073
Matthias Schmitt <github@mschmitt.info>
2017-06-18 19:08:48 +0200

    Nicer layouts

--- core/src/main/java/info/mschmitt/battyboost/core/utils/ViewBindingAdapter.java
+++ core/src/main/java/info/mschmitt/battyboost/core/utils/ViewBindingAdapter.java
@@ -11,4 +11,17 @@ public class ViewBindingAdapter {
     public static void setVisibleGone(View view, boolean show) {
         view.setVisibility(show ? View.VISIBLE : View.GONE);
     }
+
+    @BindingAdapter("onFocusGain")
+    public static void setOnNavigationItemSelected(View view, OnFocusGainListener listener) {
+        view.setOnFocusChangeListener((v, hasFocus) -> {
+            if (hasFocus) {
+                listener.onFocusGain(v);
+            }
+        });
+    }
+
+    public interface OnFocusGainListener {
+        void onFocusGain(View sender);
+    }
 }
